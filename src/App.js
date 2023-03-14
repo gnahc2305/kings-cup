7b07da61079123409d93127c7751c1b72a6eb24d
gnahc2305 <achangmurillo@gmail.com>
2023-03-13 23:31:17 -0500

    draw a random card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
-
 function App() {
+  const cards = {
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    signs: ["spades", "clubs", "diamonds", "hearts"],
+  };
+
+  function drawCard() {
+    const randomNumber = Math.floor(Math.random() * cards.numbers.length);
+    console.log(cards.numbers[randomNumber]);
+
+    const randomSign = Math.floor(Math.random() * cards.signs.length);
+    console.log(cards.signs[randomSign]);
+
+  }
+
+  drawCard();
+
   return (
-    <div className="text-3xl font-bold underline">
-      TEST
+    <div className="">
+      {/* <button className="text-3xl font-bold ml-[50%] mt-[50%] bg-blue-800 rounded-sm p-1">
+        PLAY
+      </button> */}
     </div>
   );
 }
