832dcb538c6615c5c751dfa2c2d88c04bee05174
gnahc2305 <achangmurillo@gmail.com>
2023-03-14 00:59:19 -0500

    add info about the game

---
+++ src/assets/information.js
@@ -0,0 +1,15 @@
+let cardsValue = {
+    1: 'Waterfall',
+    2: 'You',
+    3: 'Me',
+    4: 'Never Have I Ever',
+    5: 'Guys',
+    6: 'Girls',
+    7: 'Buzz',
+    8: 'Mate',
+    9: 'Rhyme',
+    10: 'Category',
+    11: 'Thumb Master',
+    12: 'Question Master',
+    13: 'Make a Rule'
+}
