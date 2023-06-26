9bfa3ea7852ed5dba706c3643dcd38df42cde11e
gnahc2305 <achangmurillo@gmail.com>
2023-06-26 14:57:18 -0500

    card now shows sign icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import spade from '../Icons/spade.png';
-import club from '../Icons/club.png';
-import diamond from '../Icons/diamond.png';
-import heart from '../Icons/heart.png';
+import React from "react";
+import spade from "../Icons/spade.png";
+import club from "../Icons/club.png";
+import diamond from "../Icons/diamond.png";
+import heart from "../Icons/heart.png";
 
-function Card({cardNumber, challenge, cardSign}) {
+function Card({ cardNumber, cardSign }) {
+  function checkSign(cardSign, cardNumber) {
+    // if (cardSign === 'spades') {
+    //   console.log('spade');
+    // }
+    switch (cardSign) {
+      case "spades":
+        return <div>{cardNumber}<img src={spade} alt='spade' height='50px' width='50px' /></div>
+      case "clubs":
+        return <div>{cardNumber}<img src={club} alt='club' height='50px' width='50px' /></div>
+      case "hearts":
+        return <div className="text-red-700">{cardNumber}<img src={heart} alt='heart' height='50px' width='50px' /></div>
+      case "diamonds":
+        return <div className="text-red-700">{cardNumber}<img src={diamond} alt='diamond' height='50px' width='50px' /></div>
+      default:
+        return;
+    }
+  }
+
   return (
-    <div className='h-[350px] w-[250px] bg-white rounded-xl'>
-      <p className='text-[50px] text-red-900 m-3'>
-        {cardNumber} {cardSign}
-      </p>
-      {/* <img src={diamond} alt="" height='64px' width='64px' /> */}
-      <p>
-        {challenge}
-      </p>
+    <div className="h-[350px] w-[250px] bg-white rounded-xl">
+      <div className="text-[70px] ml-3">
+        {/* <p className=''>{cardNumber}</p> */}
+        <div className="flex flex-col">{checkSign(cardSign, cardNumber)}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
